352ed9c102df4ec5b4e38add8e1d8043d3344a58
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-12-10 11:39:36 -0500

    [+]Social_network exercise cleared

--- social_network.js
+++ social_network.js
@@ -31,3 +31,55 @@ const usersTimeline = [
     timeline: "Yo hoy no quiero trabajar",
   },
 ];
+
+
+console.log(usersDatabase)
+
+let userName = prompt("introduce username");
+let userPass = prompt("introduce Password");
+
+function auth(usersDatabase){
+    for (let i = 0 ; i < usersDatabase.length ; i++){
+        
+            console.log(usersDatabase[i])
+            if (usersDatabase[i].username === userName & usersDatabase[i].password === userPass){
+                alert (`Welcome ${usersDatabase[i].username}`)
+                return usersDatabase[i].username                              
+            
+            }
+
+
+    }
+    alert(`We can not find this user`)
+    return false
+    
+}
+
+let authentication = auth(usersDatabase);
+
+
+function timeLine(authentication,usersDatabase,usersTimeline){
+
+    
+    for (let i = 0 ; i < usersTimeline.length ; i++){
+        if ( usersTimeline[i].username.toLowerCase() === authentication ){
+            
+
+            console.log(usersTimeline[i].username.toLowerCase())
+            
+
+
+            return usersTimeline[i].timeline
+
+
+        }
+
+    }
+
+    return `The user ${authentication} isnt loged`    
+
+
+}
+
+let social = timeLine(authentication,usersDatabase,usersTimeline)
+alert(social)
